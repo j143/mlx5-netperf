97c7cda31fd8d10d45822d05d3274bcd0cfdfb3e
deeptir <deeptir@cs.stanford.edu>
2021-08-20 06:17:56 -0700

    Sends but doesn't receive

--- main.c
+++ main.c
@@ -15,6 +15,7 @@
 
 #include <base/debug.h>
 #include <base/mem.h>
+#include <base/compiler.h>
 #include <base/mempool.h>
 #include <base/mbuf.h>
 #include <base/page.h>
@@ -29,6 +30,7 @@
 #include <infiniband/mlx5dv.h>
 /**********************************************************************/
 // CONSTANTS
+#define PORT_NUM 1 // TODO: make this dynamic
 #define NUM_BUFS_PER_PAGE 256
 #define TOTAL_MEMPOOL_PAGES 40
 #define NUM_QUEUES 1
@@ -69,6 +71,7 @@ static uint32_t client_ip;
 static uint32_t server_port = 54321; 
 static uint32_t client_port = 54321;
 
+static struct ibv_flow *eth_flow;
 static struct mempool rx_buf_mempool;
 struct mempool tx_buf_mempool;
 static struct hardware_q *rxq_out[NUM_QUEUES];
@@ -81,6 +84,7 @@ static struct ibv_pd *pd;
 static struct ibv_mr *mr_tx;
 static struct ibv_mr *mr_rx;
 static struct pci_addr nic_pci_addr;
+static uint32_t total_dropped;
 
 /**********************************************************************/
 /*
@@ -317,6 +321,8 @@ int mlx5_init_rxq(int index, struct mlx5_rxq *v) {
 	struct ibv_wq *ind_tbl[1] = {v->rx_wq};
 	struct ibv_rwq_ind_table_init_attr rwq_attr = {0};
 	rwq_attr.ind_tbl = ind_tbl;
+    rwq_attr.log_ind_tbl_size = __builtin_ctz(1);
+    rwq_attr.comp_mask = 0;
 	v->rwq_ind_table = ibv_create_rwq_ind_table(context, &rwq_attr);
 	if (!v->rwq_ind_table) {
         NETPERF_WARN("Failed to create rx indirection table");
@@ -344,6 +350,47 @@ int mlx5_init_rxq(int index, struct mlx5_rxq *v) {
         NETPERF_WARN("Failed to create rx qp");
 		return -errno;
     }
+
+    struct eth_addr *our_eth = &client_mac;
+    if (mode == UDP_SERVER) {
+        NETPERF_DEBUG("Setting eth as server_mac");
+        our_eth = &server_mac;
+    }
+
+    /* *Register sterring rules to intercept packets to our mac address and
+     * place packet in ring pointed by v->qp */
+    struct raw_eth_flow_attr {
+        struct ibv_flow_attr attr;
+        struct ibv_flow_spec_eth spec_eth;
+    } __attribute__((packed)) flow_attr = {
+        .attr = {
+            .comp_mask = 0,
+            .type = IBV_FLOW_ATTR_NORMAL,
+            .size = sizeof(flow_attr),
+            .priority = 0,
+            .num_of_specs = 1,
+            .port = PORT_NUM, // what port is this? dpdk port?
+            .flags = 0,
+        },
+        .spec_eth = {
+            .type = IBV_FLOW_SPEC_ETH,
+            .size = sizeof(struct ibv_flow_spec_eth),
+            .val = {
+                .src_mac = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+                .ether_type = 0,
+                .vlan_tag = 0,
+            },
+            .mask = {
+                .dst_mac = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF},
+                .src_mac = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+                .ether_type = 0,
+                .vlan_tag = 0,
+            }
+        }
+    };
+    rte_memcpy(&flow_attr.spec_eth.val.dst_mac, &our_eth, 6);
+    
+    
 
 	/* expose direct verbs objects */
 	struct mlx5dv_obj obj = {
@@ -407,6 +454,13 @@ int mlx5_init_rxq(int index, struct mlx5_rxq *v) {
 
 	udma_to_device_barrier();
 	wq->dbrec[0] = htobe32(v->wq_head & 0xffff);
+
+    // Do some minimal RSS stuff so we receive packets
+    eth_flow = ibv_create_flow(v->qp, &flow_attr.attr);
+    if (!eth_flow) {
+        NETPERF_ERROR("Not able to create eth_flow: %s", strerror(errno));
+        return -errno;
+    }
 
     return 0;
 }
@@ -754,6 +808,148 @@ int mlx5_transmit_one(struct mbuf *m)
 
 }
 
+void rx_completion(struct mbuf *m) {
+    // FOR NOW: do not free back to a per thread cache, just free back to the
+    // main memmpool
+    // TODO: is this the correct thing to free? unclear
+    mempool_free(&rx_buf_mempool, (void *)m->head);
+}
+
+static inline void mbuf_fill_cqe(struct mbuf *m, struct mlx5_cqe64 *cqe)
+{
+	uint32_t len;
+
+	len = be32toh(cqe->byte_cnt);
+
+	mbuf_init(m, (unsigned char *)m + RX_BUF_HEAD, len, 0);
+	m->len = len;
+
+	m->csum_type = mlx5_csum_ok(cqe);
+	m->csum = 0;
+	m->rss_hash = mlx5_get_rss_result(cqe);
+
+	m->release = rx_completion;
+}
+
+/*
+ * mlx5_refill_rxqueue - replenish RX queue with nrdesc bufs
+ * @vq: queue to refill
+ * @nrdesc: number of buffers to fill
+ *
+ * WARNING: nrdesc must not exceed the number of free slots in the RXq
+ * returns 0 on success, errno on error
+ */
+static inline int mlx5_refill_rxqueue(struct mlx5_rxq *vq, int nrdesc)
+{
+	unsigned int i;
+	uint32_t index;
+	unsigned char *buf;
+	struct mlx5_wqe_data_seg *seg;
+
+	struct mlx5dv_rwq *wq = &vq->rx_wq_dv;
+
+    // TODO: what does this assertion acutally do?
+    // Is this nrdesc must not exceed the number of free slots in the RXq?
+	NETPERF_ASSERT(wraps_lte(nrdesc + vq->wq_head, vq->consumer_idx + wq->wqe_cnt), "Wraparound assertion failed");
+
+	for (i = 0; i < nrdesc; i++) {
+        buf = mempool_alloc(&rx_buf_mempool);
+		if (unlikely(!buf)) {
+            NETPERF_ERROR("No buf left");
+			return -ENOMEM;
+        }
+
+		index = vq->wq_head++ & (wq->wqe_cnt - 1);
+		seg = wq->buf + (index << vq->rx_wq_log_stride);
+		seg->addr = htobe64((unsigned long)buf + RX_BUF_HEAD);
+		vq->buffers[index] = buf;
+	}
+
+	udma_to_device_barrier();
+	wq->dbrec[0] = htobe32(vq->wq_head & 0xffff);
+
+	return 0;
+
+}
+
+int mlx5_gather_rx(struct mbuf **ms, unsigned int budget)
+{
+	uint8_t opcode;
+	uint16_t wqe_idx;
+	int rx_cnt;
+
+	struct mlx5_rxq *v = &rxqs[0];
+	struct mlx5dv_rwq *wq = &v->rx_wq_dv;
+	struct mlx5dv_cq *cq = &v->rx_cq_dv;
+
+	struct mlx5_cqe64 *cqe, *cqes = cq->buf;
+	struct mbuf *m;
+
+	for (rx_cnt = 0; rx_cnt < budget; rx_cnt++, v->consumer_idx++) {
+		cqe = &cqes[v->consumer_idx & (cq->cqe_cnt - 1)];
+		opcode = cqe_status(cqe, cq->cqe_cnt, v->consumer_idx);
+
+		if (opcode == MLX5_CQE_INVALID) {
+			break;
+        }
+
+		if (unlikely(opcode != MLX5_CQE_RESP_SEND)) {
+            NETPERF_PANIC("got opcode %02X", opcode);
+            exit(1);
+		}
+
+        // TODO: some statistics thing we should add in later
+		total_dropped += be32toh(cqe->sop_drop_qpn) >> 24;
+        NETPERF_INFO("Total dropped: %d", total_dropped);
+
+
+		PANIC_ON_TRUE(mlx5_get_cqe_format(cqe) == 0x3, "not compressed"); // not compressed
+		wqe_idx = be16toh(cqe->wqe_counter) & (wq->wqe_cnt - 1);
+		m = v->buffers[wqe_idx];
+		mbuf_fill_cqe(m, cqe);
+		ms[rx_cnt] = m;
+	}
+
+	if (unlikely(!rx_cnt))
+		return rx_cnt;
+
+	ACCESS_ONCE(*rxq_out[0]->shadow_tail) = v->consumer_idx;
+
+	cq->dbrec[0] = htobe32(v->consumer_idx & 0xffffff);
+	PANIC_ON_TRUE(mlx5_refill_rxqueue(v, rx_cnt), "failed to refill rx queue");
+
+	return 0;
+}
+
+int check_valid_packet(struct mbuf *mbuf, void **payload_out, uint32_t *payload_len, struct eth_addr *our_eth) {
+    unsigned char *ptr = mbuf->data;
+    struct eth_hdr * const eth = (struct eth_hdr *)ptr;
+    ptr += sizeof(struct eth_hdr *);
+    struct ip_hdr * const ip = (struct ip_hdr *)ptr;
+    ptr += sizeof(struct ip_hdr *);
+    struct udp_hdr *const udp = (struct udp_hdr *)ptr;
+
+    // check if the dest eth hdr is correct
+    if (eth_addr_equal(our_eth, &eth->dhost) != 1) {
+        NETPERF_DEBUG("Bad MAC: %02" PRIx8 " %02" PRIx8 " %02" PRIx8
+			   " %02" PRIx8 " %02" PRIx8 " %02" PRIx8,
+            eth->dhost.addr[0], eth->dhost.addr[1],
+			eth->dhost.addr[2], eth->dhost.addr[3],
+			eth->dhost.addr[4], eth->dhost.addr[5]);
+        return 0;
+    }
+
+    uint16_t eth_type = ntohs(eth->type);
+    if (eth_type != ETHTYPE_IP) {
+        NETPERF_DEBUG("Bad eth type: %u", (unsigned)eth_type);
+        return 0;
+    }
+
+    // TODO: finish checks
+    return 1;
+
+}
+
 int do_client() {
     // TX window???
     struct mbuf MBUFS[32];
@@ -809,6 +1005,28 @@ int do_client() {
 }
 
 int do_server() {
+    NETPERF_DEBUG("Starting do server");
+    // poll for receiving packets
+    struct mbuf *RECV_MBUFS[32];
+    int num_received = 0;
+    while (1) {
+        num_received = mlx5_gather_rx(&RECV_MBUFS, 32);
+        if (num_received > 0) {
+            // received a packet
+            // TODO: check that the data in the mbuf is valid
+            NETPERF_DEBUG("Received packets: %d", num_received);
+            for (int  i = 0; i < num_received; i++) {
+                struct mbuf *pkt = RECV_MBUFS[i];
+                void *payload_out = NULL;
+                uint32_t payload_len = 0;
+                if (check_valid_packet(pkt, &payload_out, &payload_len, &server_mac) == 1) {
+                    NETPERF_DEBUG("Received valid pkt!");
+                }
+                mbuf_free(pkt);
+            }
+        }
+    }
+
     return 0;
 }
 
